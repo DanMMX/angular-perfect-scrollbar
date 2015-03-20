958cf99f7c464af7c0aabdd57966e69a816afe8e
Dan Rocha <bairesdev_drocha@iseatz.com>
2015-03-20 11:30:13 -0600

    Changed script AMD module alike

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -1,93 +1,95 @@
-angular.module('perfect_scrollbar', []).directive('perfectScrollbar',
-  ['$parse', '$window', '$timeout', function($parse, $window, $timeout) {
-  var psOptions = [
-    'wheelSpeed', 'wheelPropagation', 'minScrollbarLength', 'maxScrollbarLength', 'useBothWheelAxes',
-    'useKeyboard', 'suppressScrollX', 'suppressScrollY', 'scrollXMarginOffset',
-    'scrollYMarginOffset', 'includePadding'//, 'onScroll', 'scrollDown'
-  ];
+define([
+  'angular',
+  'jquery',
+  'perfectScrollbar',
+  'browserDetection'
+], function (angular, jQuery) {
+  'use strict';
 
-  return {
-    restrict: 'EA',
-    transclude: true,
-    template: '<div><div ng-transclude></div></div>',
-    replace: true,
-    link: function($scope, $elem, $attr) {
-      var oldie = function () {
-        var ua = $window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
+  angular.module('perfectScrollbar', ['app.common.services.browserDetection'])
+  .directive('perfectScrollbar', function($parse, $window, $timeout, browserDetection) {
+    var psOptions = [
+      'wheelSpeed', 'wheelPropagation', 'minScrollbarLength', 'maxScrollbarLength', 'useBothWheelAxes',
+      'useKeyboard', 'suppressScrollX', 'suppressScrollY', 'scrollXMarginOffset',
+      'scrollYMarginOffset', 'includePadding'//, 'onScroll', 'scrollDown'
+    ];
 
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-          return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))) < 10;
+    return {
+      restrict: 'EA',
+      transclude: true,
+      template: '<div><div ng-transclude></div></div>',
+      replace: true,
+      link: function($scope, $elem, $attr) {
+        if (!$elem.perfectScrollbar) {
+          $elem = jQuery($elem);
         }
 
-        return false;
-      };
-
-      if (oldie()) {
-        $elem.addClass('ps-overflow');
-        return;
-      }
-
-      var jqWindow = angular.element($window);
-      var options = {};
-
-      for (var i=0, l=psOptions.length; i<l; i++) {
-        var opt = psOptions[i];
-        if ($attr[opt] !== undefined) {
-          options[opt] = $parse($attr[opt])();
+        if (browserDetection.browser.IE && (browserDetection.browser.IE8 || browserDetection.browser.IE7)) {
+          $elem.addClass('ps-overflow');
+          return;
         }
-      }
 
-      $scope.$evalAsync(function() {
-        $elem.perfectScrollbar(options);
-        var onScrollHandler = $parse($attr.onScroll);
-        $elem.scroll(function(){
-          var scrollTop = $elem.scrollTop();
-          var scrollHeight = $elem.prop('scrollHeight') - $elem.height();
-          $scope.$apply(function() {
-            onScrollHandler($scope, {
-              scrollTop: scrollTop,
-              scrollHeight: scrollHeight
+        var jqWindow = angular.element($window);
+        var options = {};
+
+        for (var i=0, l=psOptions.length; i<l; i++) {
+          var opt = psOptions[i];
+          if ($attr[opt] !== undefined) {
+            options[opt] = $parse($attr[opt])();
+          }
+        }
+
+        $scope.$evalAsync(function() {
+          $elem.perfectScrollbar(options);
+          var onScrollHandler = $parse($attr.onScroll);
+          $elem.scroll(function(){
+            var scrollTop = $elem.scrollTop();
+            var scrollHeight = $elem.prop('scrollHeight') - $elem.height();
+            $scope.$apply(function() {
+              onScrollHandler($scope, {
+                scrollTop: scrollTop,
+                scrollHeight: scrollHeight
+              });
             });
           });
         });
-      });
 
-      function update(event) {
-        $scope.$evalAsync(function() {
-          if ($attr.scrollDown == 'true' && event != 'mouseenter') {
-            setTimeout(function () {
-              $($elem).scrollTop($($elem).prop("scrollHeight"));
-            }, 100);
-          }
+        function update(event) {
+          $scope.$evalAsync(function() {
+            if ($attr.scrollDown == 'true' && event != 'mouseenter') {
+              setTimeout(function () {
+                $($elem).scrollTop($($elem).prop("scrollHeight"));
+              }, 100);
+            }
+            $elem.perfectScrollbar('update');
+          });
+        }
+
+        // This is necessary when you don't watch anything with the scrollbar
+        $elem.bind('mouseenter', update('mouseenter'));
+
+        // Possible future improvement - check the type here and use the appropriate watch for non-arrays
+        if ($attr.refreshOnChange) {
+          $scope.$watchCollection($attr.refreshOnChange, function() {
+            update();
+          });
+        }
+
+        // this is from a pull request - I am not totally sure what the original issue is but seems harmless
+        if ($attr.refreshOnResize) {
+          jqWindow.on('resize', update);
+        }
+
+        $elem.bind('$destroy', function() {
+          jqWindow.off('resize', update);
+          $elem.perfectScrollbar('destroy');
+        });
+
+        // fixes the no scrollbar until scroll err
+        $timeout(function () {
           $elem.perfectScrollbar('update');
         });
       }
-
-      // This is necessary when you don't watch anything with the scrollbar
-      $elem.bind('mouseenter', update('mouseenter'));
-
-      // Possible future improvement - check the type here and use the appropriate watch for non-arrays
-      if ($attr.refreshOnChange) {
-        $scope.$watchCollection($attr.refreshOnChange, function() {
-          update();
-        });
-      }
-
-      // this is from a pull request - I am not totally sure what the original issue is but seems harmless
-      if ($attr.refreshOnResize) {
-        jqWindow.on('resize', update);
-      }
-
-      $elem.bind('$destroy', function() {
-        jqWindow.off('resize', update);
-        $elem.perfectScrollbar('destroy');
-      });
-
-      // fixes the no scrollbar until scroll err
-      $timeout(function () {
-        $elem.perfectScrollbar('update');
-      });
-    }
-  };
-}]);
+    };
+  });
+});
